418507a2edb28764f161c3dfd2c83be466038a09
Gustav Kullberg <gustav.kullberg@gmail.com>
2019-08-07 13:25:50 +0200

    added support for url-encoded strings

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -18,6 +18,15 @@ describe("When checking if CP1252 compatible when an invalid cp1252 string, cont
   });
 });
 
+describe("When checking if CP1252 compatible when an invalid cp1252 string, containing russian URL-encoded letters", () => {
+  const input = "%D0%9E%D0%B1%D1%80%D0%B0%D0%B7%D0%B5%D1%86";
+  it("Then returns false", done => {
+    const res = functions.textIsCP1252Compatible(input);
+    expect(res).toBe(false);
+    done();
+  });
+});
+
 describe("When checking if CP1252 compatible when an invalid cp1252 string, containing chinese letters", () => {
   const input = "假借字ظ";
   it("Then returns false", done => {
@@ -48,6 +57,15 @@ describe("When checking if string has character not in CP1252 with valid string"
 
 describe("When checking if string has character not in CP1252 with invalid string, containing russian letters", () => {
   const input = "Образец text на русском языке";
+  it("Then returns true", done => {
+    const res = functions.hasCharacterNotInCP1252(input);
+    expect(res).toBe(true);
+    done();
+  });
+});
+
+describe("When checking if string has character not in CP1252 with invalid string, containing URL-encoded russian letters", () => {
+  const input = "%D0%9E%D0%B1%D1%80%D0%B0%D0%B7%D0%B5%D1%86";
   it("Then returns true", done => {
     const res = functions.hasCharacterNotInCP1252(input);
     expect(res).toBe(true);
